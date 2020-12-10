2cedc6947c67a234ca124971436a1ebbc5ad61ca
Gary Cordero <GaryCordero1690@gmail.com>
2020-12-10 17:40:20 -0500

    leetcode problem 1436

---
+++ 1436_destination_city.js
@@ -0,0 +1,56 @@
+// 1436. Destination City
+// Easy
+
+// 435
+
+// 28
+
+// Add to List
+
+// Share
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+// It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+ 
+
+// Example 1:
+
+// Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+// Output: "Sao Paulo" 
+// Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+// Example 2:
+
+// Input: paths = [["B","C"],["D","B"],["C","A"]]
+// Output: "A"
+// Explanation: All possible trips are: 
+// "D" -> "B" -> "C" -> "A". 
+// "B" -> "C" -> "A". 
+// "C" -> "A". 
+// "A". 
+// Clearly the destination city is "A".
+// Example 3:
+
+// Input: paths = [["A","Z"]]
+// Output: "Z"
+ 
+
+// Constraints:
+
+// 1 <= paths.length <= 100
+// paths[i].length == 2
+// 1 <= cityAi.length, cityBi.length <= 10
+// cityAi != cityBi
+// All strings consist of lowercase and uppercase English letters and the space character.
+
+var destCity = function(paths) {
+  const departingCities = new Set()
+  const arrivingCities = new Set()
+  
+  for (const path of paths){
+      departingCities.add(path[0])
+      arrivingCities.add(path[1])
+  }
+  
+  return [...arrivingCities].find(x => !departingCities.has(x))
+};
